1c69d8fad2f8f50c3e304d763faeba5e2c137aa7
yoshi <irieyoshimi1@gmail.com>
2025-09-30 11:10:59 +0900

    initial commit

---
+++ spring-vocabulary/src/main/java/com/example/demo/SpringVocabularyApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringVocabularyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringVocabularyApplication.class, args);
+	}
+
+}
